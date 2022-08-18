121399723c9da9a7b0bf8ba9c764b79db5d15e69
Flavio Souza <flavioluiz.ssouza@gmail.com>
2022-08-18 19:11:45 -0300

    p2p centralized

--- praticas/lab-2/centralized/client.py
+++ praticas/lab-2/centralized/client.py
@@ -12,16 +12,17 @@ Port = int(sys.argv[2])
 server.connect((IP_address, Port))
 
 while True:
-	sockets_list = [sys.stdin, server]
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
-
-	for socks in read_sockets:
+	#sockets_list = [sys.stdin, server]
+	sockets_list = [socket.socket(), server]
+	read_sockets, write_socket, error_socket = select.select(sockets_list,[],[])
+	read = [sys.stdin, read_sockets]
+	for socks in read:
 		if socks == server:
 			message = socks.recv(2048)
 			print (message)
 		else:
 			message = sys.stdin.readline()
-			server.send(message)
+			server.send(message.encode())
 			sys.stdout.write("<You>")
 			sys.stdout.write(message)
 			sys.stdout.flush()
